0b4a4cd1534da38f446f938f07ec8ffb865f20bd
Lakaka212 <122872383+Lakaka212@users.noreply.github.com>
2023-01-17 10:50:13 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Ghost)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showIcon(IconNames.SmallHeart)
+})
